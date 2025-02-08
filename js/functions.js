1326d4b5fcfdc9c45a1d3f435eb56ae62ba59237
Alexander Kovalev <40559899+alexanderVLG@users.noreply.github.com>
2025-02-08 21:24:38 +0500

    Добавляет функции

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,27 @@
 
-const checkString = (string, length) => {
-  const stringLength = string.length;
-
-  return stringLength <= length;
-};
+const checkString = (str, maxLength) => str.length <= maxLength;
 
 const isPalindrome = (string) => {
   const words = string.replaceAll(' ', '').toLowerCase();
 
   let emptyString = '';
-
-  for(let i = words.length; i >= 0; i--) {
+  for(let i = words.length - 1; i >= 0; i--) {
     emptyString += words[i];
   }
 
-  return emptyString.slice(9) === words;
+  return emptyString === words;
 };
+
+const getNumber = (str) => {
+  const inputStr = str.toString();
+  let result = '';
+  for (let i = 0; i < inputStr.length; i++) {
+    const letter = inputStr[i];
+    const num = parseInt(letter, 10);
+    if(!Number.isNaN(num)) {
+      result += letter;
+    }
+  }
+  return parseInt(result, 10);
+};
+
