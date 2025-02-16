3d5ff36bf5186b1b3cf21e060ef0f0d615bd2fc0
Alexander Kovalev <40559899+alexanderVLG@users.noreply.github.com>
2025-02-17 00:04:15 +0500

    Удаляет комменты

--- js/main.js
+++ js/main.js
@@ -50,15 +50,6 @@ function createRandomIdFromGetRandomInteger (min, max) {
   };
 }
 
-// const getArrayComments = (el) => {
-//   const fullComments = [];
-//   for(let i = 0; i <= el.length; i++) {
-//     fullComments.push({
-//     });
-//   }
-//   return fullComments;
-// };
-
 const createArrayComments = () => {
   const quantity = getRandomInteger(0, 30);
   const arrayComment = [];
@@ -89,7 +80,3 @@ const createImage = () => {
 };
 
 const photoData = Array.from({length: 25}, createImage);
-
-console.log(photoData);
-
-
