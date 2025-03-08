57d02de6bb97a03172d2302e4103eb9947b73b05
Alexander Kovalev <40559899+alexanderVLG@users.noreply.github.com>
2025-03-08 22:09:38 +0500

    добавляет 2 слушателя

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 import {createPhotoData} from './data.js';
 import {addPictures} from './render.js';
+import {createComments} from './modal.js';
 import './modal.js';
 
 const photos = createPhotoData();
 const renderPhotos = addPictures(photos);
-
+const addComments = createComments(photos);
--- js/modal.js
+++ js/modal.js
@@ -1,9 +1,12 @@
 
-const modalWindow = document.querySelector('.big-picture');
-console.log(modalWindow);
 const mainTag = document.querySelector('main');
-console.log(mainTag);
-
+const modalWindow = document.querySelector('.big-picture');
+const closeButton = modalWindow.querySelector('.big-picture__cancel');
+const modalImage = modalWindow.querySelector('.big-picture__img');
+const modalImageLikes = modalWindow.querySelector('.likes-count');
+const commentShownCount = modalWindow.querySelector('.social__comment-shown-count');
+const commentTotalCount = modalWindow.querySelector('.social__comment-total-count');
+const commentsList = modalWindow.querySelector('.social__comments');
 
 mainTag.addEventListener('click', (evt) => {
   if(evt.target.className === 'picture__img') {
@@ -11,14 +14,24 @@ mainTag.addEventListener('click', (evt) => {
   }
 });
 
-const openPhotoModal = (image) => {
+closeButton.addEventListener('click', () => {
+  closePhotoModal();
+});
 
-  console.log(image.target.nextElementSibling);
+const openPhotoModal = (image) => {
+  modalWindow.classList.remove('hidden');
+  createImage(image);
 };
 
 const closePhotoModal = () => {
-
+  modalWindow.classList.add('hidden');
 };
 
+const createImage = (image) => {
+  modalImage.children[0].src = image.target.src;
+  modalImageLikes.textContent = image.target.nextElementSibling.lastElementChild.textContent;
+  commentShownCount.textContent = '3';
+  commentTotalCount.textContent = image.target.nextElementSibling.firstElementChild.textContent;
+};
 
-export default 'modal.js';
+export {createComments};
