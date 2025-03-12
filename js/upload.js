60bd8751582a7c8942b0f732024a1141e1c85acf
Alexander Kovalev <40559899+alexanderVLG@users.noreply.github.com>
2025-03-12 07:48:35 +0500

    Открывает и закрывает превью

--- js/upload.js
+++ js/upload.js
@@ -1,17 +1,44 @@
 const imageUploadContainer = document.querySelector('.img-upload');
 const imageUploadInput = imageUploadContainer.querySelector('.img-upload__input');
 const imageUploadOverlay = imageUploadContainer.querySelector('.img-upload__overlay');
-const imagePreview = imageUploadContainer.querySelector('.img-upload__preview');
+const closeButton = imageUploadContainer.querySelector('.img-upload__cancel');
+const imagePreview = imageUploadContainer.querySelector('.img-upload__preview img');
 const submitButton = imageUploadContainer.querySelector('.img-upload__submit');
 
-const addImage = (image) => {
-  imageUploadOverlay.classList.remove('hidden');
-  document.body.classList.add('modal-open');
+// Обработчик для клавиши Esc
+const onEscKeydown = (evt) => {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    closePreviewModal();
+  }
 };
 
-imageUploadInput.addEventListener('change', (evt) => {
-  evt.preventDefault();
-  addImage(evt.target.files[0]); // Возвращаем выбранный файл
+const closePreviewModal = () => {
+
+
+  imageUploadOverlay.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+
+  // Удаляет обработчики
+  document.removeEventListener('keydown', onEscKeydown);
+  closeButton.removeEventListener('click', closePreviewModal);
+};
+
+
+const addImage = () => {
+
+
+  imageUploadOverlay.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+
+  // Добавляет обработчики
+  document.addEventListener('keydown', onEscKeydown);
+  closeButton.addEventListener('click', closePreviewModal);
+};
+
+imageUploadInput.addEventListener('change', () => {
+
+  addImage();
 });
 
 export default 'upload.js';
