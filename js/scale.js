aaf1c8b72dfc6823f30e63cf7ee17506b0dba763
Alexander Kovalev <40559899+alexanderVLG@users.noreply.github.com>
2025-03-15 08:53:03 +0500

    Добавляет логику изменения масштаба фотографии

--- js/scale.js
+++ js/scale.js
@@ -1,19 +1,34 @@
 const scaleValue = document.querySelector('.scale__control--value');
+const imageUploadPreview = document.querySelector('.img-upload__preview');
 const decreaseButton = document.querySelector('.scale__control--smaller');
 const increaseButton = document.querySelector('.scale__control--bigger');
 
 const STEP_VALUE = 25;
 
-const calculateScale = (evt) => {
-  console.log(evt.target.classList);
+const calculateScaleMinus = () => {
+  if(parseInt(scaleValue.value, 10) < 50) {
+    return true;
+  } else {
+    scaleValue.value = `${parseInt(scaleValue.value, 10) - STEP_VALUE}%`;
+    imageUploadPreview.style.transform = `scale(${parseInt(scaleValue.value, 10) / 100})`;
+  }
 };
 
-decreaseButton.addEventListener('click', (evt) => {
-  calculateScale(evt);
+const calculateScalePlus = () => {
+  if(parseInt(scaleValue.value, 10) > 75) {
+    return true;
+  } else {
+    scaleValue.value = `${parseInt(scaleValue.value, 10) + STEP_VALUE}%`;
+    imageUploadPreview.style.transform = `scale(${parseInt(scaleValue.value, 10) / 100})`;
+  }
+};
+
+decreaseButton.addEventListener('click', () => {
+  calculateScaleMinus();
 });
 
-increaseButton.addEventListener('click', (evt) => {
-  calculateScale(evt);
+increaseButton.addEventListener('click', () => {
+  calculateScalePlus();
 });
 
 export default 'scale.js';
